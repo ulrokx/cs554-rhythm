e587b4c5aaf7b84e2e7629c4b71903e155235a64
Richard Kirk <richard.kirk790@gmail.com>
2024-05-01 16:04:57 -0400

    Add createUser event and webhooks to README

---
+++ api/src/data/users.js
@@ -0,0 +1,51 @@
+import { ObjectId } from "mongodb";
+import { users } from "./config/mongoCollections.js";
+
+const usersCollection = await users();
+
+const getUserById = async (id) => {
+  const user = await usersCollection.findOne({ _id: new ObjectId(id) });
+  if (!user) {
+    throw new Error("User not found");
+  }
+  return user;
+};
+
+export const createUser = async (user) => {
+  const userDocument = {
+    clerkId: user.id,
+    email: user.email_addresses[0].email_address,
+    name: `${user.first_name} ${user.last_name}`,
+    image: user.profile_image_url,
+    favoriteLevels: [],
+    levels: [],
+    games: [],
+    friends: [],
+    highscores: [],
+  };
+  const insertResult = await usersCollection.insertOne(userDocument);
+  if (!insertResult.insertedId) {
+    throw new Error("Failed to create user");
+  }
+  const newUser = await usersCollection.findOne({
+    _id: insertResult.insertedId,
+  });
+  return newUser;
+};
+
+export const recordGame = async (userId, levelId, players) => {
+  const user = await getUserById(userId);
+  const gameDocument = {
+    levelId,
+    players,
+  };
+  const updateResult = await usersCollection.updateOne(
+    { _id: user._id },
+    { $push: { games: gameDocument } },
+  );
+  if (updateResult.modifiedCount === 0) {
+    throw new Error("Failed to record game");
+  }
+  const newUser = await getUserById(userId);
+  return newUser;
+};
